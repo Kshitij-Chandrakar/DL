a91579a7bb31ab075b0f9d5fce21a55dd7e8313f
Kshitij-Chandrakar <chandrakarkshitij2208@gmail.com>
2026-04-02 17:42:55 +0530

    Video saving

---
+++ OpenCV/Video2.py
@@ -0,0 +1,19 @@
+import cv2 
+camera =cv2.VideoCapture(0)
+ 
+frame_width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH  ))
+frame_height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT ))
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (frame_width, frame_height))  
+while True:
+    success, frame = camera.read()
+    if not success:
+        break
+    out.write(frame)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break   
+camera.release()
+out.release()
+cv2.destroyAllWindows()
