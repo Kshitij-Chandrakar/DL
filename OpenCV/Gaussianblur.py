5967ce96e6f8037bf71638f78a2646835b036c93
Kshitij-Chandrakar <chandrakarkshitij2208@gmail.com>
2026-04-02 18:26:55 +0530

    Added Gaussaian blur high kernel

--- OpenCV/Gaussianblur.py
+++ OpenCV/Gaussianblur.py
@@ -1,7 +1,7 @@
 import cv2
 img=cv2.imread(r"E:\Workspace\free-nature-images.jpg")
 
-blur=cv2.GaussianBlur(img,(5,5),0)
+blur=cv2.GaussianBlur(img,(21,21),0)
 cv2.imshow("Original",img)  
 cv2.imshow("Blurred",blur)
 cv2.waitKey(0)
